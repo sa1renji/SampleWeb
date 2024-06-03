40c269381f3bb4d839f7a9ad3ac9d2174d1129eb
sa1renji <andrew.seromlav@gmail.com>
2024-06-03 12:59:01 +0800

    first commit

---
+++ save.php
@@ -0,0 +1,24 @@
+<?php
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Validate name
+    $input_name = trim($_POST["name"]);
+    if(empty($input_name)){
+        $name_err = "Please enter a name.";
+    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $name_err = "Please enter a valid name.";
+    } else{
+        $name = $input_name;
+    }
+    
+
+                // Records created successfully. Redirect to landing page
+                echo "Request Sent!"
+                header("location: sample.html");
+                exit();
+        
+         
+
+}
+?>
